14ad45b8ed4c291ed8e44c7e2795038aaef8320b
emanuele33 <emanuelegarcia33@gmail.com>
2017-10-19 17:05:29 -0300

    banco atualizado

--- cadastro/passos.php
+++ cadastro/passos.php
@@ -5,12 +5,13 @@
 	<title>Cadastro Passos</title>
 </head>
 <body>
-	<form action="cadastropassos" method="POST">
+	<form action="cadastropassos.php" method="POST">
 		Categoria do Tutorial: <input type="text" name="categoria">
 		Descrição do Tutorial: <input type="text" name="descricao">
 		Componentes do Tutorial: <input type="text" name="componentes">
 		Número de Passos a Seguir: <input type="number" name="numero">
-		Descrição dos Passos: <input type="text" name="passos">
+<!-- 		Descrição dos Passos: <input type="text" name="passos">
+		 -->		<input type="submit" value="Enviar">
 	</form>
 </body>
 </html>
--- php/delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-
-$id = $_GET['id'];
-unset($_SESSION['cadastro'][$id]);
-header('location:cadastro.php');
-?>
